3053852c49d55c8de47264bb640e64127dffc040
pethl <pethicklisa@gmail.com>
2017-04-10 22:08:30 +0100

    sendgrid 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,19 +82,19 @@ Rails.application.configure do
  config.action_mailer.sendgrid_actionmailer_settings = {
    api_key: ENV['SENDGRID_API_KEY']
  }
-  ActionMailer::Base.register_interceptor(SendGrid::MailInterceptor)
+ # ActionMailer::Base.register_interceptor(SendGrid::MailInterceptor)
 
-  if ENV['SENDGRID_USERNAME'] && ENV['SENDGRID_PASSWORD']
-    ActionMailer::Base.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '465',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'heroku.com',
-      :enable_starttls_auto => true,
-      :ssl => true
-    }
+ # if ENV['SENDGRID_USERNAME'] && ENV['SENDGRID_PASSWORD']
+#    ActionMailer::Base.smtp_settings = {
+#      :address        => 'smtp.sendgrid.net',
+#      :port           => '465',
+#      :authentication => :plain,
+#      :user_name      => ENV['SENDGRID_USERNAME'],
+#      :password       => ENV['SENDGRID_PASSWORD'],
+#      :domain         => 'heroku.com',
+#      :enable_starttls_auto => true,
+#      :ssl => true
+#    }
     ActionMailer::Base.delivery_method = :smtp
   end
  # config.assets.initialize_on_precompile = false
