009aaf35ad03a13cf3d4b8c6fbe5617b6d0388a2
pethl <pethicklisa@gmail.com>
2017-04-11 16:09:40 +0100

    more email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.delivery_method = :smtp 
-  config.action_mailer.raise_delivery_errors = true 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { :host => 'http://predict-the-lions.herokuapp.com' }
+   
  # config.action_mailer.delivery_method = :smtp
 # config.action_mailer.delivery_method = :sendgrid_actionmailer
 # config.action_mailer.sendgrid_actionmailer_settings = {api_key: ENV['SENDGRID_API_KEY']}
